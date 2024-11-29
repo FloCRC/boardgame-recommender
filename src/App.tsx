789e04a57b78db5b69c016532b4c53bb81e1a9db
Florent Cuer <florentcuer@gmail.com>
2024-11-29 13:58:14 +0000

    Revert "xml data retrieve for 1 entry"
    This reverts commit 58a9cebe42e0e8e33fc08e87d7a3f1a066b7fe1b.

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,8 @@
-import { useEffect, useState } from "react"
-import xmlJs from 'xml-js'
-
 function App() {
-
-    // const [xmlData, setXmlData] = useState(null)
-    const [name, setName] = useState('')
-    const [image, setImage] = useState('')
-
-    useEffect(() => {
-        fetch('http://localhost:8080/https://www.boardgamegeek.com/xmlapi2/thing?id=013,14')
-            .then((res) => {
-                return res.text()
-            })
-            .then((xmlText) => {
-                // const jsonData = xmlJs.xml2json(xmlText, { compact: true, spaces: 4 })
-                // setXmlData(JSON.parse(jsonData))
-                const parser = new DOMParser()
-                const xmlDoc = parser.parseFromString(xmlText, 'text/xml')
-                setName(xmlDoc.getElementsByTagName('name')[1].getAttribute('value'))
-                setImage(xmlDoc.getElementsByTagName('image')[0].childNodes[0].nodeValue)
-            })
-    }, [])
-
-
 
     return (
         <>
-            {/* {xmlData ? (
-                <pre>{JSON.stringify(xmlData, null, 4)}</pre>
-            ) : (
-                    <p>Loading XML data...</p>
-            )} */}
-            <p>{name}</p>
-            <img src={image} />
+            <h1>Boardgmae Recommender</h1>
         </>
     )
 }
